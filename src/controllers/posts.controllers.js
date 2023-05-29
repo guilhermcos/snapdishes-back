4b64cb130b53cd4cfab50e11d6200cbd74f15c97
guilhermcos <guigc0408@gmail.com>
2023-05-29 14:55:53 -0300

    add: feed

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -5,6 +5,37 @@ import { db } from "../database/database.connection.js";
 import { createNewPost } from "../repositories/posts.repositories.js";
 
 export default class PostsControllers {
+  async getPostsForFeed(req, res) {
+    const { userId } = res.locals;
+    try {
+      const result =
+        (
+          await db.query(
+            `
+        SELECT "users"."id" AS "userId",
+        "users"."userName" AS "userName",
+        "users"."avatarImg" AS "avatarImg",
+        "posts"."id" AS "postId",
+        "posts"."imageUrl" AS "postImgUrl",
+        "posts"."caption" AS "postCaption",
+        "likes"."userId" AS "likerId",
+        (SELECT CAST(COUNT("likes"."id") AS INTEGER) FROM likes WHERE "posts"."id" = "likes"."postId") AS "likesCount"
+        FROM users
+        JOIN follows ON "follows"."userId" = users.id
+        JOIN posts ON "posts"."userId" = "users"."id"
+        LEFT JOIN likes ON "likes"."userId" = "follows"."followerUserId" AND "likes"."postId" = "posts"."id"
+        WHERE "follows"."followerUserId" = $1
+        ORDER BY "posts"."id" DESC
+      `,
+            [userId]
+          )
+        ).rows ?? [];
+      res.status(200).send(result);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
+
   async createPost(req, res) {
     const file = req.file;
     const { userId } = res.locals;
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -3,7 +3,7 @@ import PostsControllers from "../controllers/posts.controllers.js";
 import multer from "multer";
 import tokenValidation from "../middlewares/token.validation.js";
 
-const postControllers = new PostsControllers();
+const postsControllers = new PostsControllers();
 const upload = multer({ dest: "uploads/" });
 
 const postsRouter = Router();
@@ -12,7 +12,8 @@ postsRouter.post(
   "/posts/create",
   tokenValidation,
   upload.single("image"),
-  postControllers.createPost
+  postsControllers.createPost
 );
+postsRouter.get("/posts/feed", tokenValidation, postsControllers.getPostsForFeed);
 
 export default postsRouter;
